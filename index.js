db75f979cf0f2d294978fb5a86bcbf35cfc0458d
Jesper Lekland <jesper.lekland@gmail.com>
2017-07-13 14:49:19 +0200

    remove RN logic from index.js

--- index.js
+++ index.js
@@ -1,11 +1,4 @@
-// eslint-disable-next-line no-unused-vars
-import React from 'react'
-import { AppRegistry } from 'react-native'
-import App from './app'
-
 export { default as LineChart } from './line-chart'
 export { default as PieChart } from './pie-chart'
 export { default as AreaChart } from './area-chart'
 export { default as BarChart } from './bar-chart'
-
-AppRegistry.registerComponent('d3Playground', () => App)
