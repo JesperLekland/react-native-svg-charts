513d978d5e743d560daefae09a276f5bb07bf27b
Jesper Lekland <jesper.lekland@gmail.com>
2018-02-04 18:10:28 +0100

    update stories

--- storybook/stories/gradient.js
+++ storybook/stories/gradient.js
@@ -1,6 +1,6 @@
 import React from 'react'
-import AreaChart from '../../src/area-chart'
-import { LinearGradient, Stop } from 'react-native-svg'
+import { AreaChart, Path } from 'react-native-svg-charts'
+import { Defs, LinearGradient, Stop } from 'react-native-svg'
 
 class GradientExample extends React.PureComponent {
 
@@ -8,23 +8,40 @@ class GradientExample extends React.PureComponent {
 
         const data = [ 50, 10, 40, 95, -4, -24, 85, 91, 35, 53, -53, 24, 50, -20, -80 ]
 
+        const Gradient = ({ index }) => (
+            <Defs key={index}>
+                <LinearGradient id={'gradient'} x1={'0%'} y={'0%'} x2={'0%'} y2={'100%'}>
+                    <Stop offset={'0%'} stopColor={'rgb(134, 65, 244)'} stopOpacity={0.8}/>
+                    <Stop offset={'100%'} stopColor={'rgb(134, 65, 244)'} stopOpacity={0.2}/>
+                </LinearGradient>
+                <LinearGradient id={'line-gradient'} x1={'0%'} y={'0%'} x2={'0%'} y2={'100%'}>
+                    <Stop offset={'0%'} stopColor={'rgb(134, 65, 244)'} stopOpacity={0.8}/>
+                    <Stop offset={'100%'} stopColor={'rgb(134, 65, 244)'} stopOpacity={0.2}/>
+                </LinearGradient>
+            </Defs>
+        )
+
+        const Line = ({ line }) => (
+            <Path
+                key={'line'}
+                d={line}
+                fill={'none'}
+                stroke={'url(#line-gradient)'}
+            />
+        )
+
+        const extras = [
+            Gradient,
+            Line,
+        ]
+
         return (
             <AreaChart
-                style={ { height: 200 } }
-                dataPoints={ data }
-                contentInset={ { top: 20, bottom: 20 } }
-                renderGradient={ ({ id }) => (
-                    <LinearGradient id={ id } x1={ '0%' } y={ '0%' } x2={ '0%' } y2={ '100%' }>
-                        <Stop offset={ '0%' } stopColor={ 'rgb(134, 65, 244)' } stopOpacity={ 0.8 }/>
-                        <Stop offset={ '100%' } stopColor={ 'rgb(134, 65, 244)' } stopOpacity={ 0.2 }/>
-                    </LinearGradient>
-                ) }
-                renderLineGradient={ ({ id }) => (
-                    <LinearGradient id={ id } x1={ '0%' } y={ '0%' } x2={ '0%' } y2={ '100%' }>
-                        <Stop offset={ '0%' } stopColor={ 'rgb(134, 65, 244)' } stopOpacity={ 0.8 }/>
-                        <Stop offset={ '100%' } stopColor={ 'rgb(134, 65, 244)' } stopOpacity={ 0.2 }/>
-                    </LinearGradient>
-                ) }
+                style={{ height: 200 }}
+                dataPoints={data}
+                contentInset={{ top: 20, bottom: 20 }}
+                extras={extras}
+                svg={{ fill: 'url(#gradient)' }}
             />
         )
     }
