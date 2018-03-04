31c327ebd21d53bcf160399887fb9407e1654895
Jesper Lekland <jesper.lekland@gmail.com>
2018-03-04 18:51:18 +0100

    fix bar gradient example

--- storybook/stories/gradient-bar.js
+++ storybook/stories/gradient-bar.js
@@ -1,44 +1,31 @@
 import React from 'react'
-import { LinearGradient, Stop } from 'react-native-svg'
+import { LinearGradient, Stop, Defs } from 'react-native-svg'
 import { BarChart } from 'react-native-svg-charts'
 
 class GradientBarExample extends React.PureComponent {
 
     render() {
 
-        const {
-                  fillColor         = 'rgb(134, 65, 244)',
-                  fillColorNegative = 'rgb(239, 71, 71)',
-              } = this.props
-
         const data    = [ 50, 10, 40, 95, -4, -24, 85, 91, 35, 53, -53, 24, 50, -20, -80 ]
-        const barData = [
-            {
-                values: data,
-                positive: {
-                    fill: fillColor,
-                    // other react-native-svg supported props
-                },
-                negative: {
-                    fill: fillColorNegative,
-                    // other react-native-svg supported props
-                },
-            },
-        ]
+
+        const Gradient = () => (
+            <Defs key={'gradient'}>
+                <LinearGradient id={'gradient'} x1={'0%'} y={'0%'} x2={'0%'} y2={'100%'}>
+                    <Stop offset={'0%'} stopColor={'rgb(134, 65, 244)'}/>
+                    <Stop offset={'100%'} stopColor={'rgb(66, 194, 244)'}/>
+                </LinearGradient>
+            </Defs>
+        )
 
         return (
             <BarChart
                 style={ { height: 200 } }
-                data={ barData }
+                data={ data }
                 contentInset={ { top: 20, bottom: 20 } }
-                renderGradient={ ({ id, value, fill }) => (
-                    <LinearGradient id={ id } x1={ '0%' } y={ '0%' } x2={ '0%' } y2={ '100%' }>
-                        <Stop offset={ '0%' } stopColor={ value > 0 ? fill : 'rgb(66, 194, 244)' }/>
-                        <Stop offset={ '100%' } stopColor={ value < 0 ? fill : 'rgb(66, 194, 244)' }/>
-                    </LinearGradient>
-                ) }
+                extras={[ Gradient ]}
                 svg={ {
                     strokeWidth: 2,
+                    fill: 'url(#gradient)',
                 } }
             />
         )
