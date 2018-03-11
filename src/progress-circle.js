ac6cdf14d589052caf1b64cae13193db46307bbd
Jesper Lekland <jesper.lekland@gmail.com>
2018-03-11 18:11:57 +0100

    fix indentation

--- src/progress-circle.js
+++ src/progress-circle.js
@@ -21,8 +21,8 @@ class ProgressCircle extends PureComponent {
         const {
                   style,
                   progressColor,
-            backgroundColor,
-            strokeWidth,
+                  backgroundColor,
+                  strokeWidth,
                   startAngle,
                   endAngle,
                   animate,
