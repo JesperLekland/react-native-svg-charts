85685d9da6962f82f267423561c4b63e7d29b1b9
Andy Cloke <apcloke@gmail.com>
2019-04-23 16:51:53 +0100

    Fix blinking animation
    Animation blinks to new value, then animates, because the d prop is passed through to the path, before being animated by _animate

--- src/animated-path.js
+++ src/animated-path.js
@@ -85,6 +85,7 @@ class AnimatedPath extends Component {
             <Path
                 ref={ ref => this.component = ref }
                 { ...this.props }
+                d={this.state.d}
             />
         )
     }
