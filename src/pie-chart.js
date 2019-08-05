8574007d8748493ffcc908e7730897d4e2832f5b
Christian Roring <christian@athom.nl>
2019-08-05 14:06:27 +0200

    Added a fix for iOS not working with the pointerEvents on the SVG tag

--- src/pie-chart.js
+++ src/pie-chart.js
@@ -1,5 +1,5 @@
 import React, { PureComponent } from 'react'
-import { View } from 'react-native'
+import { View, Platform } from 'react-native'
 import PropTypes from 'prop-types'
 import * as shape from 'd3-shape'
 import Svg, { G, Path } from 'react-native-svg'
@@ -128,7 +128,7 @@ class PieChart extends PureComponent {
             <View pointerEvents={'box-none'} style={style}>
                 <View pointerEvents={'box-none'} style={{ flex: 1 }} onLayout={(event) => this._onLayout(event)}>
                     {height > 0 && width > 0 && (
-                        <Svg pointerEvents={'box-none'} style={{ height, width }}>
+                        <Svg pointerEvents={Platform.OS === 'android' && 'box-none'} style={{ height, width }}>
                             {/* center the progress circle*/}
                             <G x={width / 2} y={height / 2}>
                                 {React.Children.map(children, (child) => {
