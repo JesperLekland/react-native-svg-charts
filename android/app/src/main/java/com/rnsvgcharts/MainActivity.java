f023f9d8dcd26cf2d0f096ec9af161f5d31fd84c
JB Theard <jbtheard@sportsos.tech>
2020-04-02 15:18:33 +0100

    chore: fix android studio config
    Now tested with latest Android Studio and RN 0.62

--- android/app/src/main/java/com/rnsvgcharts/MainActivity.java
+++ android/app/src/main/java/com/rnsvgcharts/MainActivity.java
@@ -10,6 +10,6 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "RnSvgCharts";
+    return "react-native-svg-charts";
   }
 }
